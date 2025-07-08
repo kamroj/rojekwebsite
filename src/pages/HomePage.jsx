122ae2298d5874226f11460fca558445079cdb8a
Kamil Rojek <kamroj@gmail.com>
2025-07-08 11:17:41 +0200

    Add gradient to gallery

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -98,8 +98,15 @@ const HomePage = () => {
         />
       </Section>
       
-      {/* Realizacje section with dark background */}
-      <Section dark label="REALIZACJE">
+      {/* Realizacje section with custom gradient background */}
+      <Section 
+        dark 
+        label="REALIZACJE"
+        customStyles={`
+          background: linear-gradient(323deg,rgba(0, 0, 0, 1) 0%, rgba(9, 20, 10, 1) 86%, rgba(13, 31, 15, 1) 100%);
+        `}
+        noPadding
+      >
         <RealizationsGallery 
           images={realizationData}
           options={{
